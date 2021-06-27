dc542739c0dcd797cb3e646444cab81c17a2c060
Denis Volk <denis.volk@gmail.com>
2021-06-27 13:10:30 +0000

    install.sh fixes

--- install.sh
+++ install.sh
@@ -7,6 +7,8 @@ cd
 # install conda
 wget https://repo.anaconda.com/miniconda/Miniconda3-py39_4.9.2-Linux-x86_64.sh -O ~/miniconda.sh
 bash ~/miniconda.sh -b -p $HOME/miniconda
+./miniconda/bin/conda init bash
+source .bashrc
 
 # clone catsup
 git clone https://github.com/oxfordmmm/catsup
@@ -15,7 +17,6 @@ cd catsup
 
 # install conda environment
 conda env create -f environment.yml
-conda activate catsup
 
 # download minikraken2 database into catsup/
 wget https://objectstorage.uk-london-1.oraclecloud.com/n/lrbvkel2wjot/b/sp3_deps/o/minikraken2_v2_8GB_201904.tgz --content-disposition
